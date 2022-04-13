cc6a554d2d3eab5f3efba1132b3f23abd5865b7a
Fatih Mehmet Baydar <baydarmf@gmail.com>
2022-04-13 21:27:12 +0300

    Create Solution.java

---
+++ Prepare/Java/01 - Introduction/02 - Java Stdin and Stdout I/Solution.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(scanner.nextInt());
+        }
+    }
+}
